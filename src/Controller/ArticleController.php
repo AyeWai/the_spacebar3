93d1e251d42cf0a17a9d82060378ad5853e99a2f
Chris SIMON <c.simon@groupetenor.com>
2020-03-12 08:46:42 +0100

    commit before 7

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,9 +3,10 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
-class ArticleController
+class ArticleController extends AbstractController
 {   
     /**
      * @Route("/") 
@@ -19,7 +20,16 @@ class ArticleController
      */
     public function show($slug)
     {
-        return new Response(sprintf('Futur page to show one space article : "%s"', $slug));
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+        return $this->render('article/show.html.twig', 
+        ['title' => ucwords(str_replace('-', ' ', $slug)),
+        'comments' => $comments,
+        ]);
     }
 }
 
